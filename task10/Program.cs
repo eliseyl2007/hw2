3c741e49a2a836ff050ff7cf1134d74649af5e73
Elisei <elisey-l@mail.ru>
2023-09-15 18:00:04 +0300

    10

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трех значное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numb = num / 10;
+int number = numb % 10;
+Console.WriteLine($"вторая цифра ->{number}");
