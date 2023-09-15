51d20e69e281fa03ccdb0ef4f990dd435a92511e
Elisei <elisey-l@mail.ru>
2023-09-15 18:00:45 +0300

    13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+    while (num > 999)
+    {
+    num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine($"Третья цифра ->{num}");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
