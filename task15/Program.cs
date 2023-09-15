4898ef7279076d350f4e2f80763ab10ff516938c
Elisei <elisey-l@mail.ru>
2023-09-15 18:01:15 +0300

    15

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = DayOff (num);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool DayOff(int num)
+{
+    return(num == 6 || num ==7);
+}
